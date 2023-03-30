cb718f1cdc1716294c1ae40d869ca39d264206ef
Amer Ahmed <amer.m.ahmed345@gmail.com>
2023-03-30 02:53:32 +0200

    Updated main.py

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def ad_scraper(number_of_times, list_of_keywords):
     """Returns a list of keywords ads for a given number of times"""
     # Specify User Agent
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.87 Safari/537.36",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36 Edg/92.0.902.78",
     }
 
     # progress bar for scraping
